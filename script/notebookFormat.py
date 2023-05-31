cfff1ca18b32002a1a3fece31462a3011b577d39
Savakar <rohansavakar@gmail.com>
2023-05-31 14:45:26 +0530

    the result printing

--- script/notebookFormat.py
+++ script/notebookFormat.py
@@ -5,7 +5,19 @@ directory = 'C:\\Users\\rohan\\OneDrive\\Desktop\\StockWhiz\\exampleFiles\\files
 
 for filename in os.listdir(directory):
     if filename.endswith('.ipynb'):
+        # Show which file presently working on 
         print(filename)
-        with open(os.path.join(directory, filename), 'r+', encoding='utf-8') as file:
+        with open(os.path.join(directory, filename), 'w+', encoding='utf-8') as file:
             data = json.load(file)
-            # print(file.name)
+            # Looping through the cells below
+            for cell in data.cells:
+                # Each cell has a source object
+                # We will only edit the cells which are of type code
+                if cell.cell_type == "code":
+                    # Inside the source
+                    # print('inside source')
+                    source = cell.source
+                    if isinstance(source,str):
+                        # The source element is a string
+                        result = [s + "\n" for s in source.split("\n")] 
+                        print(result)
